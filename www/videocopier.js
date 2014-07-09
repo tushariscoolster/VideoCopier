3213a1d597c8ea62ababf54a609cd54d9c08b4fe
Tushar  Borole <tushariscoolster@gmail.com>
2014-07-09 16:51:49 +0530

    Update videocopier.js

--- www/videocopier.js
+++ www/videocopier.js
@@ -1,15 +1,13 @@
-cordova.define("cordova/plugin/videocopier",
-  function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var VideoCopier = function () {};
+function VideoCopier() {
+}
 
-    /**
+ /**
      * Copy video file from Asset folder to internal storage so that it can be played through a standard HTML5 <video> tag
      *
      * @param url           URL of the file to be copied
      */
     VideoCopier.prototype.copy = function(url, callback) {
-		exec( 	function(result) {
+		cordova.exec( 	function(result) {
 					callback(result);	// Returns URL to a copy in internal storage (as a String)
 				},
 			  	function(result) {
@@ -21,13 +19,13 @@ cordova.define("cordova/plugin/videocopier",
 			);
     };
 
-    var videoCopier = new VideoCopier();
-    module.exports = videoCopier;
-});
-
-if (!window.plugins) {
+VideoCopier.install = function () {
+  if (!window.plugins) {
     window.plugins = {};
-}
-if (!window.plugins.videoCopier) {
-    window.plugins.videoCopier = cordova.require("cordova/plugin/videocopier");
-}
+  }
+
+  window.plugins.videoCopier = new VideoCopier();
+  return window.plugins.videoCopier;
+};
+
+cordova.addConstructor(VideoCopier.install);
